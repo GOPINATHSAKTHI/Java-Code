a989b26fdd51d233ef4bb7b39966e3969cedce29
GOPINATHSAKTHI <gopinaths_bit27@mepcoeng.ac.in>
2026-01-28 11:35:44 +0530

    Implement Adder class extending Arithmetic

---
+++ inheritance2.java
@@ -0,0 +1,23 @@
+class Arithmetic
+{ 
+    int add(int x, int y)
+    { 
+        return x+y; 
+    } 
+}
+
+class Adder extends Arithmetic
+{
+
+}
+
+public class Solution {
+
+public static void main(String[] args) {
+    System.out.println("My superclass is: Arithmetic"); 
+    Adder a = new Adder();
+    System.out.print(a.add(12, 30)+" ");
+    System.out.print(a.add(10, 3)+" ");
+    System.out.print(a.add(10, 10)+" ");
+}
+}
